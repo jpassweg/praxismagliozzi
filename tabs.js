9c0cfdfb841b812e5740a3ddba48ddf3120ace90
Jonas Passweg <jonas.passweg@gmail.com>
2019-09-21 18:37:03 +0200

    earlier transition to mobile berufserfahrung

--- tabs.js
+++ tabs.js
@@ -1,7 +1,7 @@
 function updateTab() {
     tabsize = 170;
 	screensize = $(window).width();
-	leftPadding = ((screensize - 808) / 2);
+	leftPadding = ((screensize - 828) / 2);
 
 	$("#BerufserfahrungBig").css("padding-left", leftPadding + "px");
 
@@ -9,7 +9,7 @@ function updateTab() {
         var newpos = tabsize;
         $(this).css("left", newpos + "px");
         $(this).css("top", -30 + "px")
-		$(this).css("width", (808 - tabsize) + "px");
+		$(this).css("width", (828 - tabsize) + "px");
 	});
 }
 
